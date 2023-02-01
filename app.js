856fd947f308fdfaec8f3cd09a9f78bf006479af
Rocio Marucco <coderociomarucco@gmail.com>
2023-01-31 21:01:06 -0300

    dom

---
+++ app.js
@@ -0,0 +1,16 @@
+const contenedorProductos = document.getElementById('contenedor-productos')
+
+stockProductos.forEach((producto) => {
+    const div = document.createElement ('div')
+    div.classList.add('producto')
+    div.innerHTML = `
+    <img src=${producto.img} alt="">
+    <h3>${producto.nombre}</h3>
+    <p> Presentación: ${producto.presentacion} </p>
+    <p class="precioProducto">Precio:$ ${producto.precio}</p>
+    <button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
+    `
+
+    contenedorProductos.appendChild(div)
+})
+
---
+++ stock.js
@@ -0,0 +1,10 @@
+let stockProductos = [
+    {id: aceiteEsencialCien, nombre: "Aceite Esencial", cantidad: 30, precio: 1000, presentacion: "100 ml", img: './imagenes/aceiteLavanda.jpg'},
+    {id: velaAromaticaChica, nombre: "Vela Aromática", cantidad: 5, precio: 700, presentacion: "Chica", img: './imagenes/velaAromatica.jpg'},
+    {id: velaAromaticaGrande, nombre: "Vela Aromática", cantidad: 10, precio: 1500, presentacion: "Grande", img: './imagenes/velaAromatica.jpg'},
+    {id: aguaMicelar, nombre: "Agua Micelar", cantidad: 6, precio: 1800, presentacion: "200 ml", img: './imagenes/aguaMicelar.jpg'},
+    {id: ramoFloresChico, nombre: "Ramo de Flores Secas", cantidad: 20, precio: 400, presentacion: "Chica", img: './imagenes/ramoDeFloresSecas.jpg'},
+    {id: ramoFloresGrande, nombre: "Ramo de Flores Secas", cantidad: 5, precio: 700, presentacion: "Grande", img: './imagenes/ramoDeFloresSecas.jpg'},]
+
+
+  
