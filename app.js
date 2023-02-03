671933f8be8c53048f0b1c472ab15c952c8f9268
Rocio Marucco <coderociomarucco@gmail.com>
2023-02-02 21:10:33 -0300

    Tercer pre Entega

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
+let carrito = []
+
 const contenedorProductos = document.getElementById('contenedor-productos')
 
 stockProductos.forEach((producto) => {
     const div = document.createElement ('div')
     div.classList.add('producto')
     div.innerHTML = `
-    <img src=${producto.img} alt="">
+    <img src=${producto.img} class= "imgTarjeta" alt="">
     <h3>${producto.nombre}</h3>
     <p> Presentación: ${producto.presentacion} </p>
-    <p class="precioProducto">Precio:$ ${producto.precio}</p>
-    <button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
+    <p class="precioProducto">Precio: $ ${producto.precio}</p>
+    <button id="agregar${producto.id}" class="boton-agregar">AGREGAR AL CARRITO</button>
     `
 
     contenedorProductos.appendChild(div)
+
+    const boton = document.getElementById(`agregar${producto.id}`)
+
+    boton.addEventListener ('click', () => {
+        agregarAlCarrito(producto.id)
+    })
 })
+
+const agregarAlCarrito = (productoId) => {
+    const item = stockProductos.find ((producto) => producto.id === productoId)
+    carrito.push (item)
+    console.log (carrito)
+}
 
--- stock.js
+++ stock.js
@@ -1,10 +1,11 @@
 let stockProductos = [
-    {id: aceiteEsencialCien, nombre: "Aceite Esencial", cantidad: 30, precio: 1000, presentacion: "100 ml", img: './imagenes/aceiteLavanda.jpg'},
-    {id: velaAromaticaChica, nombre: "Vela Aromática", cantidad: 5, precio: 700, presentacion: "Chica", img: './imagenes/velaAromatica.jpg'},
-    {id: velaAromaticaGrande, nombre: "Vela Aromática", cantidad: 10, precio: 1500, presentacion: "Grande", img: './imagenes/velaAromatica.jpg'},
-    {id: aguaMicelar, nombre: "Agua Micelar", cantidad: 6, precio: 1800, presentacion: "200 ml", img: './imagenes/aguaMicelar.jpg'},
-    {id: ramoFloresChico, nombre: "Ramo de Flores Secas", cantidad: 20, precio: 400, presentacion: "Chica", img: './imagenes/ramoDeFloresSecas.jpg'},
-    {id: ramoFloresGrande, nombre: "Ramo de Flores Secas", cantidad: 5, precio: 700, presentacion: "Grande", img: './imagenes/ramoDeFloresSecas.jpg'},]
+    {id: 1, nombre: "Aceite Esencial", cantidad: 30, precio: 1000, presentacion: "100 ml", img: './imagenes/aceiteLavanda.jpg'},
+    {id: 2, nombre: "Vela Aromática", cantidad: 5, precio: 700, presentacion: "Chica", img: './imagenes/velaAromatica.jpg'},
+    {id: 3, nombre: "Vela Aromática", cantidad: 10, precio: 1500, presentacion: "Grande", img: './imagenes/velaAromatica.jpg'},
+    {id: 4, nombre: "Agua Micelar", cantidad: 6, precio: 1800, presentacion: "200 ml", img: './imagenes/aguaMicelar.jpg'},
+    {id: 5, nombre: "Ramo de Flores Secas", cantidad: 20, precio: 400, presentacion: "Chica", img: './imagenes/ramoDeFloresSecas.jpg'},
+    {id: 6, nombre: "Ramo de Flores Secas", cantidad: 5, precio: 700, presentacion: "Grande", img: './imagenes/ramoDeFloresSecas.jpg'},]
 
 
-  
+guardarLocal("stockProductos", JSON.stringify(stockProductos))
+
